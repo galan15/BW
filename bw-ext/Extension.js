d07e61f31e06fcda3c18de0ebfdfed565be67e23
Piotr Galanek <piotr.galanek@outlook.com>
2022-10-29 19:51:51 +0200

    Remove duplicated code

--- bw-ext/Extension.js
+++ bw-ext/Extension.js
@@ -1,17 +1,3 @@
-NodeList.prototype.last = function () {
-    return this[this.length - 1];
-};
-
-String.prototype.normalize = function () {
-    return this?.toLowerCase() ?? '';
-};
-
-String.prototype.capitalizeEachWord = function () {
-    return this.split(' ')
-        .map(word => word.charAt(0).toUpperCase() + word.slice(1))
-        .join(' ');
-};
-
 class Extension {
     static instance = Extension.instance ?? new Extension();
 
