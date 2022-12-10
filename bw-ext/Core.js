cf7a7c490067430d08aa7e95958d28b9dcfab347
Michał Rocławski <michal.roclawski@ihsmarkit.com>
2022-12-10 16:10:25 +0100

    feat(auction): refactor of higlighting items

--- bw-ext/Core.js
+++ bw-ext/Core.js
@@ -127,7 +127,7 @@ class Item {
 
 class Scrap extends Item {
     static plnPrice = 20000;
-    static prPrice = 1700;
+    static prPrice = 1900;
 
     constructor(node, name, price) {
         super(node, name, price, []);
